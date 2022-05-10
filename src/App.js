e2efef21004bf884b453da55af474e25c3c69c4f
Royal <royalbai9@gmail.com>
2022-05-10 10:48:50 -0400

    Fix this

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ import { useState, useEffect } from "react";
 
 function App() {
 	const [earthquakes, setEarthquakes] = useState([]);
-	const [incidentsHero1, setIncidentsHero1] = useState("");
-	const [incidentsHero2, setIncidentsHero2] = useState("");
-	const [incidentsHero3, setIncidentsHero3] = useState("");
-	const [incidentsHero4, setIncidentsHero4] = useState("");
 
 	// Firebase Database
 	useEffect(() => {
@@ -25,23 +21,13 @@ function App() {
 
 			for (let key in data) {
 				newState.push({
-					key: key,
-					hero1: data[key].hero1,
-					hero2: data[key].hero2,
-					hero3: data[key].hero3
+					
 				});
 			}
 
 			setEarthquakes(newState);
 		})
 	}, [])
-
-	// Incidents Database
-	const incidentsReport = {
-		hero1: incidentsHero1,
-		hero2: incidentsHero2,
-		hero3: incidentsHero3,
-	}
 
 	return (
 		<div className="App">
