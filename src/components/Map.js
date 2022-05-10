249954e81275620eade483942cd7ca4e52d1dada
Gabriel Wright <gabriel.wright@ryerson.ca>
2022-05-10 16:14:47 -0400

    marker color change is working correctly

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,7 @@ import MapMarker from "./MapMarker";
 
 const Map = ({ earthquakesData }) => {
 	const markerList = earthquakesData;
-	const markerListShort = markerList.slice(60, 100);
+	// const markerListShort = markerList.slice(60, 100);
 
 	return (
 		<MapContainer
@@ -23,7 +23,7 @@ const Map = ({ earthquakesData }) => {
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			{markerListShort.map((marker) => {
+			{markerList.map((marker) => {
 				// grab the lat and long
 				const incidentLon = marker.geometry.coordinates[0];
 				const incidentLat = marker.geometry.coordinates[1];
