08a55bacddf6e97c96696bb8d0562e195990b7b4
Gabriel Wright <gabriel.wright@ryerson.ca>
2022-05-12 16:03:31 -0400

    add error handling pseudocode for MVP features

--- src/App.js
+++ src/App.js
@@ -30,8 +30,7 @@ function App() {
 
 	const startDate = "2022-05-05"; //start tracking from project start date
 	let today = new Date();
-	let userDate = (today.toLocaleDateString("en-US"));
-	
+	let userDate = today.toLocaleDateString("en-US");
 
 	//function to get earthquake data from USGS API.
 	//startTime argument passed to the function, limits data to events on or after the specified start time
@@ -45,29 +44,47 @@ function App() {
 				starttime: startTime,
 				eventtype: "earthquake",
 				minmagnitude: 0,
-				// orderby:"magnitude",
+				orderby: "time",
+				limit: 20000,
 			},
 		})
 			.then((response) => {
 				const listOfEarthquakes = response.data.features;
 				setEarthquakesData(listOfEarthquakes);
+				console.log(listOfEarthquakes);
 			})
+
+			// Error 2: USGS API Call fails
+			// When this happens, alert the user with the error message
+			// It tells the user, what part of the app failed
+			// IT tells the user, what to do next (hard-refresh: CTRL/CMD + SHIFT + R)
+
+			// Error 3: API response array item limit reached (20,000 response objects)
+			// When this happens, alert the user with the error message "Limit reached, data as far back as X"
+			// If the limit is reached, grab the item at the end of the response object array,
+			// target its time property, convert the time into a date, and then render that date
+			// to the page as X, for example: "Total earthquake incidents since X"
+
 			.catch((err) => console.log(err));
 	};
 
 	useEffect(() => {
 		const fetchData = async () => {
 			function sleep(ms) {
-				return new Promise(resolve => setTimeout(resolve, ms));
+				return new Promise((resolve) => setTimeout(resolve, ms));
 			}
 
 			await loadDataToFirebase(earthquakesData);
 			await sleep(1000);
 			await getDataFromFirebase();
+			// Error 1: API loading indicator
+			// When page loads, isLoading state value = true
+			// Once the functions above are done running (3 promises), toggle isLoading state = false
+			// When this happens, conditionally render "loading" modal/element
 
 			const allTotalCount = getTotals(earthquakesData);
 			setTotalCount(allTotalCount);
-		}
+		};
 		fetchData();
 	}, [earthquakesData]);
 
@@ -85,11 +102,15 @@ function App() {
 	}, []);
 
 	// Firebase Database
-	const loadDataToFirebase = async(earthquakesData) => {
+	const loadDataToFirebase = async (earthquakesData) => {
 		const database = getDatabase(firebase);
 		const dbRef = ref(database, `/incidents/${startDate}`);
 
 		set(dbRef, earthquakesData);
+		// Error 4: Firebase "set" method fails
+		// Wrap the "set" method in a try...catch code block
+		// If the try succeeds (i.e. sets the earthquake data at the dbRef), do nothing
+		// If the try fails, capture the error, and alert the user indicating that the firebase "set" failed
 	};
 
 	const getHeroesSummary = () => {
@@ -114,15 +135,18 @@ function App() {
 				totalIncidents: totalCount.allTeam,
 				incidentsOver24Hrs: todaysCount.allTeam,
 			},
-    	];
+		];
 		setHeroesSummary(summary);
-	}
-	
-	
+	};
 
-	const getDataFromFirebase = async() => {
+	const getDataFromFirebase = async () => {
 		const database = getDatabase(firebase);
 		const dbRef = ref(database, `/incidents/${startDate}`);
+
+		// Error 5: Firebase "get" method fails
+		// Wrap the get method in a try...catch block
+		// If the try succeeds (i.e. sets the earthquake data at the dbRef), do nothing
+		// If the try fails, capture the error, and alert the user indicating that the firebase "set" failed
 
 		// Getting Data from Firebase
 		get(dbRef).then((snapshot) => {
@@ -130,21 +154,22 @@ function App() {
 			const copyOfFirebaseData = [...firebaseData];
 
 			// Filtering through the Data
-			const todaysEarthquakeData = copyOfFirebaseData.filter((incident) => {
-				// Converting the time
-				let rawDate = new Date(incident.properties.time);
-				let convertedDate = (rawDate.toLocaleDateString("en-US"));
+			const todaysEarthquakeData = copyOfFirebaseData.filter(
+				(incident) => {
+					// Converting the time
+					let rawDate = new Date(incident.properties.time);
+					let convertedDate = rawDate.toLocaleDateString("en-US");
 
-				return userDate === convertedDate;
-			});
+					return userDate === convertedDate;
+				}
+			);
 			setTodaysEarthquakeData(todaysEarthquakeData);
-		})
-	}
-	
-	const getTotals = (earthquakesData) => {
+		});
+	};
 
+	const getTotals = (earthquakesData) => {
 		const copyOfEarthQuakeData = [...earthquakesData];
-		
+
 		const teal = copyOfEarthQuakeData.filter((incident) => {
 			const incidentMag = incident.properties.mag;
 			if (incidentMag < 3) {
@@ -172,25 +197,23 @@ function App() {
 				return incidentMag;
 			}
 		});
-		
+
 		const heroTotals = {
 			geoTeacher: teal.length,
 			richMoral: blue.length,
 			strongGoode: purple.length,
 			allTeam: darkPurple.length,
-		}
+		};
 		return heroTotals;
-	}
+	};
 
 	return (
 		<div className="App">
 			<h1>Hello world!</h1>
-			
 
 			<Map earthquakesData={todaysEarthquakeData} />
-			<TotalEarthquakeDisplay heroesSummary={heroesSummary}/>
-			<TodaysEarthquakeDisplay heroesSummary={heroesSummary}/>
-
+			<TotalEarthquakeDisplay heroesSummary={heroesSummary} />
+			<TodaysEarthquakeDisplay heroesSummary={heroesSummary} />
 		</div>
 	);
 }
