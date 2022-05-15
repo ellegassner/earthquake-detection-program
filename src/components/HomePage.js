a77ca984fd0d7bef013a56e203647161c29c7ac6
Royal <royalbai9@gmail.com>
2022-05-15 09:01:54 -0400

    Emmet json settings - Created baseStyles sass file - Styled HomePage partial

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,18 @@
 import { Route, Routes, Link, useParams } from 'react-router-dom';
+import HomePageImg from "../assets/homepage-three-shields.png"
 
 
 const HomePage = () => {
     return (
-        <>
-            <h1>Smoothie Hello!</h1>
-            <Link to='/questionspage'>Questions</Link>
-        </>
+        <div className='homePage'>
+            <div className="wrapper">
+                <h1><span className='left-align'>the</span><span className='title'>extraordinary collective of superheroes</span><span className='right-align'>are here to help</span></h1>
+                <div>
+                    <img src={HomePageImg} alt="Home page three shields" />
+                </div>
+                <Link to='/questionspage'>Learn what we do</Link>
+            </div>
+        </div>
     )
 }
 
